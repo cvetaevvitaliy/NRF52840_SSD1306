2e04195818089e984f8f78ee70e7f9d21393a8a1
cvetaevvitaliy <cvetaevvitaliy@gmail.com>
2020-08-23 00:58:45 +0300

    upload

---
+++ main.c
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2009 - 2020, Nordic Semiconductor ASA
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form, except as embedded into a Nordic
+ *    Semiconductor ASA integrated circuit in a product or a software update for
+ *    such product, must reproduce the above copyright notice, this list of
+ *    conditions and the following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ *
+ * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * 4. This software, with or without modification, must only be used with a
+ *    Nordic Semiconductor ASA integrated circuit.
+ *
+ * 5. Any software provided in binary form under this license must not be reverse
+ *    engineered, decompiled, modified and/or disassembled.
+ *
+ * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+/** @file
+* @brief Example template project.
+* @defgroup nrf_templates_example Example Template
+*
+*/
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "sdk_config.h"
+#include "nrf.h"
+#include "nordic_common.h"
+#include "boards.h"
+#include "nrf_delay.h"
+#include "ssd1306.h"
+
+static void BlinkLED(void){
+
+    for (int i = 1; i < 3; i++){
+            bsp_board_led_invert(i);
+            nrf_delay_ms(250);
+        }
+
+}
+
+
+int main(void)
+{
+    bsp_board_init(BSP_INIT_LEDS);
+    i2c_init();
+    ssd1306_Init();
+    ssd1306_FlipScreenVertically();
+    ssd1306_Clear();
+    ssd1306_Draw_String("For Kaa IoT ",0, 0, &Font_8x10);
+    ssd1306_Draw_String("NRF52840",0, 11, &Font_8x10);
+    ssd1306_UpdateScreen();
+    while (true)
+    {
+        BlinkLED();
+        
+    }
+}
+
